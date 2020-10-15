50b7564b7ee4ee256f7b84df345834576bc4023d
anasmansouri <htmlphpcss3@gmail.com>
2020-10-15 18:57:36 +0100

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants.dart';
-/*import 'package:web_app/sections/about/about_section.dart';
-import 'package:web_app/sections/contact/contact_section.dart';
-import 'package:web_app/sections/feedback/feedback_section.dart';
-import 'package:web_app/sections/recent_work/recent_work_section.dart';
-import 'package:web_app/sections/service/service_section.dart';*/
+import 'package:my_portfolio/sections/about/about_section.dart';
+//import 'package:my_portfolio/sections/contact/contact_section.dart';
+//import 'package:my_portfolio/sections/feedback/feedback_section.dart';
+import 'package:my_portfolio/sections/recent_work/recent_work_section.dart';
+import 'package:my_portfolio/sections/service/service_section.dart';
 import 'package:my_portfolio/sections/topSection/top_section.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,17 +15,17 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             TopSection(),
-            //SizedBox(height: kDefaultPadding * 2),
-            //AboutSection(),
-            //ServiceSection(),
-            //RecentWorkSection(),
+            SizedBox(height: kDefaultPadding * 2),
+            AboutSection(),
+            ServiceSection(),
+            RecentWorkSection(),
             //FeedbackSection(),
             //SizedBox(height: kDefaultPadding),
             // ContactSection(),
             // This SizeBox just for demo
-            // SizedBox(
-            //   height: 500,
-            // )
+            SizedBox(
+              height: 500,
+            )
           ],
         ),
       ),
